3734f464061b3152f9ba047df5c00dbde247e7af
decaf_cp <SND\decaf_cp>
2011-06-02 12:31:02 -0700

    Merge Dev => Main Documentation and version updates for 1.2

--- Main/Source/GlobalAssemblyInfo.cs
+++ Main/Source/GlobalAssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Resources;
 [assembly: AssemblyCopyright("Copyright © Peter Palotas 2008-2011")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("1.2.3000.0")]
-[assembly: AssemblyFileVersion("1.2.3000.0")]
+[assembly: AssemblyVersion("1.2.3000.1")]
+[assembly: AssemblyFileVersion("1.2.3000.1")]
